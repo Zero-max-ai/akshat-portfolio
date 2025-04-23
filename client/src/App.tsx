12a9871814e91ffb27588ecfdd5555aa9340337d
Zero-max-ai <gangi.keshav03@gmail.com>
2025-04-23 23:34:50 +0530

    removed all zustand code.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import useThemeStore from './store/theme';
 
 const App = () => {
-  const theme = useThemeStore((state) => state.theme);
-  useEffect(() => {
-    const root = document.documentElement((state) => state.theme);
-    if(theme === 'dark') {
-      root.classList.add('dark');
-    } else {
-      root.classList.remove('dark');
-    }
-  }, [theme])
-
   return (
     <Routes>
       <Route path={'/'} element={<Layout />}>
